fb78fdf21e2d683f55c043c80c89868cf94e2525
spdys <noorii549@yahoo.com>
2025-02-11 20:54:38 +0300

    Started step 5 explore Dune TODO: figure out how to round the double before returning it

--- desert_adventure.kt
+++ desert_adventure.kt
@@ -4,6 +4,7 @@ fun main() {
     println(currentTemperature(25, 40))
     println(startCamelRide(10, 5))
     println(startCamelRide(10))
+    println(exploreDune(climbRate = 3, height = 20))
 }
 
 fun greetTraveller() {
@@ -20,4 +21,9 @@ fun currentTemperature(morningTemperature: Int, afternoonTemperature: Int): Stri
 
 fun startCamelRide(time: Int, speed: Int = 5): String {
     return "Camel Ride: Distance ${time*speed} km, Speed = $speed km/h, Time = $time hours"
+}
+
+fun exploreDune(height: Int, climbRate: Int): String {
+    var time: Double = height.toDouble()/climbRate.toDouble()
+    return "Time to Climb the Dune: $time hours"
 }
