2459af85d2a032bbc2c71f31d0dda7e86278a5f3
spdys <noorii549@yahoo.com>
2025-02-11 20:48:09 +0300

    Finished step 4 with the camel ride calculation including the speed having a default int value of 5

--- desert_adventure.kt
+++ desert_adventure.kt
@@ -2,6 +2,8 @@ fun main() {
     greetTraveller()
     println(findOasis(10, 20))
     println(currentTemperature(25, 40))
+    println(startCamelRide(10, 5))
+    println(startCamelRide(10))
 }
 
 fun greetTraveller() {
@@ -14,4 +16,8 @@ fun findOasis(x: Int, y: Int): String {
 
 fun currentTemperature(morningTemperature: Int, afternoonTemperature: Int): String {
     return "Average Temperature: ${(morningTemperature+afternoonTemperature)/2}"
+}
+
+fun startCamelRide(time: Int, speed: Int = 5): String {
+    return "Camel Ride: Distance ${time*speed} km, Speed = $speed km/h, Time = $time hours"
 }
